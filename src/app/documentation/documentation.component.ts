8ac34a231df5d6a1a638e4ff47606ede9c7ce7a7
mcourteix <me.courteix@gmail.com>
2018-12-04 16:30:52 +0100

    documentation page

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-documentation',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentationComponent implements OnInit {
 
-  constructor() { }
+  public allDocumentation: any;
 
-  ngOnInit() {
+  constructor(private _firebase: AngularFireDatabase) { }
+
+  async ngOnInit() {
+    // Récupération des données firebase
+    this._firebase.object('/documentation').valueChanges().subscribe(data => {
+      this.allDocumentation = data;
+
+      console.log('this.allDocumentation', this.allDocumentation);
+
+    });
   }
 
 }
