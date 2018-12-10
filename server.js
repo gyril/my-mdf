52838791246fa8b50b427377dec1e83ddd1a40a4
Cyril Gantzer <cyril@frontapp.com>
2018-12-09 17:54:06 -0800

    :shirt:

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const port = process.env.PORT || 7000;
 const express = require('express');
 const app = express();
 app.use(express.static('dist/my-mdf'));
-app.listen(port, () => console.log(`Server started on port ${port}!`))
+app.listen(port, () => console.log(`Server started on port ${port}!`));
